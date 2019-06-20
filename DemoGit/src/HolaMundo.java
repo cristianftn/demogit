7dbff5c9d565f605a4f3ab12a504af3aab788d0f
cristian <cristianftn@gmail.com>
2019-06-20 09:39:59 +0200

    www

--- DemoGit/src/HolaMundo.java
+++ DemoGit/src/HolaMundo.java
@@ -3,7 +3,7 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("hola mundo");
+System.out.println("hola gente");
 	}
 
 }
